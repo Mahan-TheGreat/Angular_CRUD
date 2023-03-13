ca7046208d5646fab8d4f1a717998cffb3e62eb2
mahan <mahan.timalsena@danphecare.com>
2023-03-13 20:58:25 +0545

    Added Required Imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TableComponent } from './components/table/table.component';
+
+import { ToastModule } from 'primeng/toast';
+import { ToolbarModule } from 'primeng/toolbar';
+import { TableModule } from 'primeng/table';
+import { DialogModule } from 'primeng/dialog';
+import { ReactiveFormsModule } from '@angular/forms';
+import {FileUploadModule} from 'primeng/fileupload';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { ProductService } from './services/product.service';
+import { HttpClientModule } from '@angular/common/http';
+import { InputTextModule } from 'primeng/inputtext';
+
+
 
 @NgModule({
   declarations: [
@@ -12,9 +26,23 @@ import { TableComponent } from './components/table/table.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    InputTextModule,
+    ToastModule,
+    ToolbarModule,
+    TableModule,
+    DialogModule,
+    FileUploadModule
   ],
-  providers: [],
+  providers: [
+    ConfirmationService,
+    MessageService,
+    ProductService
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
