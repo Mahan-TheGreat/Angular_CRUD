3f97326d6be6ffab54d089d7ff11e5c63c2760db
mahan <mahan.timalsena@danphecare.com>
2023-03-20 21:18:22 +0545

    Added Image in the table

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -4,6 +4,7 @@ import { MessageService } from 'primeng/api';
 import { FileUpload } from 'primeng/fileupload';
 import { ImageDTO } from 'src/app/interface/image';
 import { product } from 'src/app/interface/product';
+import { ImageService } from 'src/app/services/ImageService';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -20,9 +21,7 @@ export class TableComponent implements OnInit{
   header = "Edit Product";
   isAddProduct = false;
   isFileSelected = false;
-
   isDiscountAvailable = false;
-
   fileNameBase64 = '';
 
   imageDTO  = {
@@ -38,6 +37,7 @@ export class TableComponent implements OnInit{
   quantity: 0,
   isDiscountApplicable: false,
   percentDiscount: 0,
+  imagePath: '',
   isActive: true
   }
   editProductForm = new FormGroup({
@@ -46,7 +46,9 @@ export class TableComponent implements OnInit{
     price: new FormControl(0,[Validators.required,Validators.min(0)]),
     quantity: new FormControl(0,[Validators.required,Validators.min(0)]),
     isDiscountApplicable: new FormControl('',[Validators.required]),
-    percentDiscount: new FormControl(0,[Validators.required,Validators.min(0)])  });
+    percentDiscount: new FormControl(0,[Validators.required,Validators.min(0)]),
+    image: new FormControl('')
+  });
 
 get Name() {
     return this.editProductForm.get('name');
@@ -67,7 +69,14 @@ get Quantity() {
 get PercentDiscount() {
   return this.editProductForm.get('percentDiscount');
 }
-constructor(private _messageService: MessageService, private _productService: ProductService){
+get Image(){
+  return this.editProductForm.get('image');
+}
+constructor(
+  private _messageService: MessageService,
+  private _productService: ProductService,
+  private _imageService: ImageService
+   ){
 
 }
 
@@ -87,7 +96,18 @@ private GetAllProducts(){
   })
 }
 addProduct(){
-        this._productService.addProduct(this.selectedProduct)
+  const product1 = {
+    id:0,
+    name: this.selectedProduct.name,
+    description: this.selectedProduct.description,
+    price: this.selectedProduct.price,
+    quantity: this.selectedProduct.quantity,
+    isDiscountApplicable: this.selectedProduct.isDiscountApplicable,
+    percentDiscount: this.selectedProduct.percentDiscount,
+    imagePath: new ImageDTO(this.imageDTO),
+    isActive: true
+  }
+        this._productService.addProduct(product1)
              .subscribe({
           next: res => {
             console.log(res)
@@ -100,8 +120,18 @@ addProduct(){
       })
 }
 updateProduct(){
-  console.log(this.selectedProduct)
-  this._productService.updateProduct(this.selectedProduct.id, this.selectedProduct)
+  const product = {
+    id: this.selectedProduct.id,
+    name: this.selectedProduct.name,
+    description: this.selectedProduct.description,
+    price: this.selectedProduct.price,
+    quantity: this.selectedProduct.quantity,
+    isDiscountApplicable: this.selectedProduct.isDiscountApplicable,
+    percentDiscount: this.selectedProduct.percentDiscount,
+    imagePath: new ImageDTO(this.imageDTO),
+    isActive: this.selectedProduct.isActive
+  }
+  this._productService.updateProduct(product.id, product)
       .subscribe({
           next: res => {
                   this._messageService.add({ severity: 'success', summary: 'Successful', detail: 'Product Updated', life: 3000 });
@@ -114,21 +144,31 @@ updateProduct(){
 }
 
 deactivateData(selectedProduct: product) {
-  this.selectedProduct.id = selectedProduct.id;
-  this.selectedProduct.name = selectedProduct.name;
-  this.selectedProduct.isActive = false;
-  // this.imageDTO.filePath = selectedProduct.logoPath;
+  console.log(selectedProduct)
+  this._productService.disableProduct(selectedProduct.id)
+  .subscribe({
+    next: res=>{
+      this._messageService.add({ severity: 'success', summary: 'Successful', detail: 'Product Deactivated.', life: 3000 });
+      this.GetAllProducts();
 
-  this.updateProduct();
+    },
+    error: err => {
+      this._messageService.add({ severity: 'error', summary: 'Error', detail: 'Deactivation failed! Please try again.', life: 3000 });
+    }
+  })
 }
 
 activateData(selectedProduct: product) {
-  this.selectedProduct.id = selectedProduct.id;
-  this.selectedProduct.name = selectedProduct.name;
-  this.selectedProduct.isActive = true;
-  // this.imageDTO.filePath = selectedProduct.logoPath;
-
-  this.updateProduct();
+  this._productService.enableProduct(selectedProduct.id)
+  .subscribe({
+    next: res=>{
+      this._messageService.add({ severity: 'success', summary: 'Successful', detail: 'Product Activated.', life: 3000 });
+      this.GetAllProducts();
+    },
+    error: err => {
+      this._messageService.add({ severity: 'error', summary: 'Error', detail: 'Activation failed! Please try again.', life: 3000 });
+    }
+  })
 }
 
 addNew() {
@@ -146,8 +186,10 @@ showDialog($event: any) {
   this.editProductForm.get('quantity')?.patchValue(this.selectedProduct.quantity);
   this.editProductForm.get('isDiscountApplicable')?.patchValue(JSON.stringify(this.selectedProduct.isDiscountApplicable!));
   this.editProductForm.get('percentDiscount')?.patchValue(this.selectedProduct.percentDiscount!);
+  this.editProductForm.get('image')?.patchValue(this.selectedProduct.imagePath!);
+
   this.isDiscountAvailable = this.selectedProduct.isDiscountApplicable!;
-  // this.imageDTO.filePath = this.selectedProduct.logoPath;
+  this.imageDTO.filePath = this.selectedProduct.imagePath;
   this.showEditPopup = true;
 
 }
@@ -162,10 +204,10 @@ checkNull() {
     isNull = true;
     return isNull;
   }
-  // if((this.isAddProduct && !this.isFileSelected)){
-  //     isNull = true;
-  //     return isNull;
-  // }
+  if((this.isAddProduct && !this.isFileSelected)){
+      isNull = true;
+      return isNull;
+  }
   return isNull;
 }
 save() {
@@ -175,15 +217,16 @@ save() {
   }
   console.log(this.editProductForm.value)
   //for Image upload
-    // if (!this.isFileSelected) {
-    //   this.imageDTO.filePath = this.editProductForm.value.logo!;
-    // } 
+    if (!this.isFileSelected) {
+      this.imageDTO.filePath = this.editProductForm.value.image!;
+    } 
       this.selectedProduct.name = this.editProductForm.value.name!;
       this.selectedProduct.description = this.editProductForm.value.description!;
       this.selectedProduct.price = this.editProductForm.value.price!;
       this.selectedProduct.quantity = this.editProductForm.value.quantity!;
       this.selectedProduct.isDiscountApplicable = JSON.parse(this.editProductForm.value.isDiscountApplicable!);
       this.selectedProduct.percentDiscount = this.editProductForm.value.percentDiscount!;
+      this.selectedProduct.imagePath = this.editProductForm.value.image!;
 
       if (!this.isAddProduct) {
           this.updateProduct();
@@ -206,6 +249,7 @@ resetSelectedProduct(){
     quantity: 0.5,
     isDiscountApplicable: false,
     percentDiscount: 0,
+    imagePath: '',
     isActive: true
   }
 }
@@ -226,10 +270,10 @@ async handleFileChange() {
   this.isFileSelected = true;
   const file = this.ImageUploader.files[0];
   
-  // await this._editDataService.convertImagetoBase64(file)
-  //     .then(
-  //         res => fileBase64Url = res
-  //     );
+  await this._imageService.convertImagetoBase64(file)
+      .then(
+          res => fileBase64Url = res
+      );
   const fileBase64 = fileBase64Url.split(',')[1];
   this.imageDTO.fileName = file.name;
   this.imageDTO.base64String = fileBase64;
@@ -244,6 +288,10 @@ handleChange(e: any){
   this.editProductForm.patchValue({
       isDiscountApplicable: e.target.value
   }) 
+}
+removeFileFromPopup() {
+  this.isFileSelected = false;
+  this.imageDTO = {fileName:'',filePath:'',base64String:""}
 }
 
 }
