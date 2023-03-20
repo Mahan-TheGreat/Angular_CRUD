f4f0fd5b16efffc4236dec1de8a7612f9bdbd945
mahan <mahan.timalsena@danphecare.com>
2023-03-20 21:22:32 +0545

    Added Api calls to disable and enable product

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { enviroment } from 'src/enviroment';
+import { postProduct } from '../interface/postProduct';
 import { product } from '../interface/product';
 
 @Injectable({
@@ -18,11 +19,19 @@ export class ProductService{
     return this.http.get<product[]>(`${this.baseUrl}Products/GetProduct`);
   }
 
-  addProduct(product: product){
+  addProduct(product: postProduct){
     return this.http.post(`${this.baseUrl}Products/AddProduct`,product);
   }
 
-  updateProduct(id: number, product:product){
+  updateProduct(id: number, product:postProduct){
     return this.http.put(`${this.baseUrl}Products/EditProduct/${id}`,product)
+  }
+
+  disableProduct(id: number){
+    return this.http.put(`${this.baseUrl}Products/DisableProduct/${id}`,null)
+  }
+
+  enableProduct(id: number){
+    return this.http.put(`${this.baseUrl}Products/EnableProduct/${id}`,null)
   }
 }
